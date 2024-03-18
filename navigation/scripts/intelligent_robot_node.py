dc9fed7487e62f1eb699eda373bb775cb57df504
sm1393 <swapnilmane1393@gmail.com>
2024-03-18 22:36:20 +0530

    Each robot can subscribe to every other robot's pose (For simulation purpose only)

--- navigation/scripts/intelligent_robot_node.py
+++ navigation/scripts/intelligent_robot_node.py
@@ -19,30 +19,33 @@ arucoParams = cv2.aruco.DetectorParameters_create()
 myGoal = PoseWithCovarianceStamped()
 bridge = CvBridge()
 
-robotID = rospy.myargv(argv=sys.argv)[1]
+robotID = int(rospy.myargv(argv=sys.argv)[1])
 
-rospy.init_node('robot_' + robotID + '_node')
-client = actionlib.SimpleActionClient('/volta_' + robotID + '/move_base',MoveBaseAction)
+rospy.init_node('volta_' + str(robotID) + '_node')
+client = actionlib.SimpleActionClient('/volta_' + str(robotID) + '/move_base',MoveBaseAction)
 client.wait_for_server()
 goal = MoveBaseGoal()
 goal.target_pose.header.frame_id = 'map'
 
+noOfRobots = 4
+myLeaderID = 0
 robotsAroundMe = set()
 frontCamera = False
 leftCamera = False
 rightCamera = False
 
-mainLeaderPose = np.array([0.0, 0.0])
-def mainLeaderPoseCallback(msg):
-    global mainLeaderPose
-    mainLeaderPose[0] = msg.pose.pose.position.x
-    mainLeaderPose[1] = msg.pose.pose.position.y
+class robotInfo:
+    def __init__(self, id):
+        self.id = id
+        self.pose = np.array([0.0, 0.0])
+        rospy.Subscriber("/volta_" + str(self.id) + "/amcl_pose", PoseWithCovarianceStamped, self.poseCallback)
 
-myPose = np.array([0.0, 0.0])
-def myPoseCallback(msg):
-    global myPose
-    myPose[0] = msg.pose.pose.position.x
-    myPose[1] = msg.pose.pose.position.y
+    def poseCallback(self, msg):
+        global myGoal, myLeaderID
+        if self.id == myLeaderID:
+            myGoal = msg
+        self.pose[0] = msg.pose.pose.position.x
+        self.pose[1] = msg.pose.pose.position.y
 
 def frontCameraCallback(msg):
     global frontCamera, robotsAroundMe
@@ -79,46 +82,33 @@ def selectMyLeader():
     leftCamera = False
     rightCamera = False
 
-    tempRobotPose = np.array([0.0, 0.0])
-    def neighbourRobotPoseCallback(msg):
-        tempRobotPose[0] = msg.pose.pose.position.x
-        tempRobotPose[1] = msg.pose.pose.position.y
-
     minDistance = np.inf
     myLeaderID = np.nan
     for robot in robotsAroundMe:
         if robot == 0:
             return 0
-        robotPoseSub = rospy.Subscriber("/volta_" + str(robot) + "/amcl_pose", PoseWithCovarianceStamped, neighbourRobotPoseCallback)
-        time.sleep(1)
-        distance = np.linalg.norm(tempRobotPose - myPose)
+        distance = np.linalg.norm(robotsInfo[robot].pose - robotsInfo[robotID].pose)
         if distance < minDistance:
             minDistance = distance
             myLeaderID = robot
-        robotPoseSub.unregister()
     robotsAroundMe = set({})
     return myLeaderID
 
-myLeaderPose = np.array([0.0, 0.0])
-myLeaderPrevPose = np.array([0.0, 0.0])
-def myLeaderPoseCallback(msg):
-    global myLeaderPose, myGoal
-    myGoal = msg
-    myLeaderPose[0] = msg.pose.pose.position.x
-    myLeaderPose[1] = msg.pose.pose.position.y
+myLeaderPrevPose = np.array([np.inf, np.inf])
 
 if __name__ == '__main__':
     try:
-        rospy.Subscriber("/volta_0/amcl_pose", PoseWithCovarianceStamped, mainLeaderPoseCallback)
-        rospy.Subscriber("/volta_" + robotID + "/amcl_pose", PoseWithCovarianceStamped, myPoseCallback)
-        rospy.Subscriber("/volta_" + robotID + "/camera_front/camera_front", Image, frontCameraCallback)
-        rospy.Subscriber("/volta_" + robotID + "/camera_left/camera_left", Image, leftCameraCallback)
-        rospy.Subscriber("/volta_" + robotID + "/camera_right/camera_right", Image, rightCameraCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_front/camera_front", Image, frontCameraCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_left/camera_left", Image, leftCameraCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_right/camera_right", Image, rightCameraCallback)
+
+        robotsInfo = []
+        for id in range(noOfRobots):
+            robotsInfo.append(robotInfo(id))
 
         time.sleep(1)
         myLeaderID = selectMyLeader()
         print("myLeaderID = ", myLeaderID)
-        myLeaderPoseSub = rospy.Subscriber("/volta_" + str(myLeaderID) + "/amcl_pose", PoseWithCovarianceStamped, myLeaderPoseCallback)
         print("Ready to GO !!!")
 
         while not rospy.is_shutdown():
@@ -128,8 +118,8 @@ if __name__ == '__main__':
                 leftCamera = False
                 rightCamera = False
                 robotsAroundMe = set({})
-            if np.linalg.norm(myLeaderPose-myPose) > 1:
-                if np.linalg.norm(myLeaderPose-myLeaderPrevPose) > 1:
+            if np.linalg.norm(robotsInfo[myLeaderID].pose - robotsInfo[robotID].pose) > 1:
+                if np.linalg.norm(robotsInfo[myLeaderID].pose - myLeaderPrevPose) > 1:
                     goal.target_pose.header.stamp = rospy.Time.now()
                     goal.target_pose.pose.position.x = myGoal.pose.pose.position.x
                     goal.target_pose.pose.position.y = myGoal.pose.pose.position.y
@@ -139,7 +129,7 @@ if __name__ == '__main__':
                     goal.target_pose.pose.orientation.z = myGoal.pose.pose.orientation.z
                     goal.target_pose.pose.orientation.w = myGoal.pose.pose.orientation.w
                     client.send_goal(goal)
-                    myLeaderPrevPose = np.copy(myLeaderPose)
+                    myLeaderPrevPose = np.copy(robotsInfo[myLeaderID].pose)
             else:
                 client.stop_tracking_goal()
                 client.cancel_all_goals()
