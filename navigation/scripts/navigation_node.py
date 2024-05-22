44f114a4722d609eef25618fb18eaed6e0e0caad
sm1393 <swapnilmane1393@gmail.com>
2024-05-22 20:08:55 +0530

    Updating conditions for navigation

--- navigation/scripts/navigation_node.py
+++ navigation/scripts/navigation_node.py
@@ -9,9 +9,10 @@ import rospy
 import actionlib
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from navigation.msg import navigation
+from nav_msgs.msg import Path
 
 myGoal = PoseWithCovarianceStamped()
+localPlanCopy = Path()
 navigationControl = False
 isRobotDeviated = False
 
@@ -41,16 +42,29 @@ def myPoseCallback(msg):
     myPose[0] = msg.pose.pose.position.x
     myPose[1] = msg.pose.pose.position.y    
 
-def navigationCallback(msg):
-    global navigationControl, isRobotDeviated
-    navigationControl = msg.flag
-    isRobotDeviated = msg.deviated
+def localPlanCallback(msg):
+    global localPlanCopy
+    localPlanCopy = msg
+
+def navigationControl():
+    global localPlanCopy, myPose
+    pointOnLine = np.array([0.0, 0.0])
+    minDeviationFromPath = np.inf
+    for pose in localPlanCopy.poses:
+        pointOnLine[0] = pose.pose.position.x
+        pointOnLine[1] = pose.pose.position.y
+        deviation = np.linalg.norm(pointOnLine - myPose)
+        if deviation < minDeviationFromPath:
+            minDeviationFromPath = deviation
+        if minDeviationFromPath > 0.25:
+            return True
+    return False
 
 if __name__ == '__main__':
     try:
         rospy.Subscriber("/volta_" + str(robotID) + "/amcl_pose", PoseWithCovarianceStamped, myPoseCallback)
         rospy.Subscriber("/volta_" + str(myLeaderID) + "/amcl_pose", PoseWithCovarianceStamped, leaderPoseeCallback)
-        rospy.Subscriber("/volta_" + str(robotID) + "/navigation", navigation, navigationCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/move_base/TebLocalPlannerROS/local_plan", Path, localPlanCallback)
 
         time.sleep(1)
         print(robotID, "following", myLeaderID)
@@ -65,17 +79,17 @@ if __name__ == '__main__':
             goal.target_pose.pose.orientation.y = myGoal.pose.pose.orientation.y
             goal.target_pose.pose.orientation.z = myGoal.pose.pose.orientation.z
             goal.target_pose.pose.orientation.w = myGoal.pose.pose.orientation.w
+            if np.linalg.norm(leaderPose - myPose) < 2:
+                client.stop_tracking_goal()
+                client.cancel_all_goals()
+                continue
             if np.linalg.norm(leaderPose - leaderPrevPose) > 1:
-                if np.linalg.norm(leaderPose - myPose) > 1:
-                    if client.get_state() == 1 and not navigationControl:
-                        client.cancel_all_goals()
-                        client.send_goal(goal)
-                    else:
-                        client.send_goal(goal)
-                else:
-                    client.stop_tracking_goal()
-                    client.cancel_all_goals()
+                client.send_goal(goal)
                 leaderPrevPose = np.copy(leaderPose)
+            if navigationControl():
+                print("Deviated")
+            else:
+                print("On the path")
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
