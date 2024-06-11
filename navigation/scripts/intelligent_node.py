913359d632eba0d767ef4c320d3b19d31eced064
sm1393 <swapnilmane1393@gmail.com>
2024-06-11 16:41:19 +0530

    Added camera node

--- navigation/scripts/intelligent_node.py
+++ navigation/scripts/intelligent_node.py
@@ -3,30 +3,80 @@
 import sys
 
 import numpy as np
+from cv_bridge import CvBridge
+import cv2
 import time
+import imutils
 
 import rospy
+from sensor_msgs.msg import Image
 
+class Camera:
+    def __init__(self, noOfRobots):
+        self.robotsAroundMe = set()
+        self.frontCamera = False
+        self.leftCamera = False
+        self.rightCamera = False
+        self.noOfRobots = noOfRobots
+        
+        self.arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_250)
+        self.arucoParams = cv2.aruco.DetectorParameters_create()
+        self.bridge = CvBridge()
 
-robotID = int(rospy.myargv(argv=sys.argv)[1])
+    def frontCameraCallback(self, msg):
+        image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        corners, ids, rejected = cv2.aruco.detectMarkers(image, self.arucoDict, parameters=self.arucoParams)
+        image = imutils.resize(image, width=1000)
+        if type(ids) != type(None):
+            for id in ids:
+                if id in range(self.noOfRobots):
+                    self.robotsAroundMe.add(id[0])
+        self.frontCamera = True
 
-rospy.init_node('volta_' + str(robotID) + '_intelligent_node')
+    def leftCameraCallback(self, msg):
+        image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        corners, ids, rejected = cv2.aruco.detectMarkers(image, self.arucoDict, parameters=self.arucoParams)
+        image = imutils.resize(image, width=1000)
+        if type(ids) != type(None):
+            for id in ids:
+                if id in range(self.noOfRobots):
+                    self.robotsAroundMe.add(id[0])
+        self.leftCamera = True
 
-myPose = np.array([0.0, 0.0])
-def myPoseCallback(msg):
-    global myPose
-    myPose[0] = msg.pose.pose.position.x
-    myPose[1] = msg.pose.pose.position.y    
+    def rightCameraCallback(self, msg):
+        image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        corners, ids, rejected = cv2.aruco.detectMarkers(image, self.arucoDict, parameters=self.arucoParams)
+        image = imutils.resize(image, width=1000)
+        if type(ids) != type(None):
+            for id in ids:
+                if id in range(self.noOfRobots):
+                    self.robotsAroundMe.add(id[0])
+        self.rightCamera = True
 
 if __name__ == '__main__':
     try:
-        # rospy.Subscriber("/volta_" + str(robotID) + "/amcl_pose", PoseWithCovarianceStamped, myPoseCallback)
+        robotID = int(rospy.myargv(argv=sys.argv)[1])
+        rospy.init_node('volta_' + str(robotID) + '_camera_node')
+
+        noOfRobots = 4
+        camera = Camera(noOfRobots)
+
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_front/camera_front", Image, camera.frontCameraCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_left/camera_left", Image, camera.leftCameraCallback)
+        rospy.Subscriber("/volta_" + str(robotID) + "/camera_right/camera_right", Image, camera.rightCameraCallback)
 
         time.sleep(1)
 
         while not rospy.is_shutdown():
-            # print("Goal state = ", client.get_state())
-            pass
+            if camera.frontCamera and camera.leftCamera and camera.rightCamera:
+                robotsAroundMeCopy = camera.robotsAroundMe.copy()
+                print(robotsAroundMeCopy)
+                collisionPossible = False
+                camera.frontCamera = False
+                camera.leftCamera = False
+                camera.rightCamera = False
+                robotsAroundMe = set({})
+                robotsAroundMeCopy = set({})
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
