647e29f86d56f25edae60f48d4829d78fd266356
sm1393 <swapnilmane1393@gmail.com>
2024-06-14 00:53:52 +0530

    Added Camera node

--- navigation/scripts/intelligent_node.py
+++ navigation/scripts/intelligent_node.py
@@ -9,7 +9,19 @@ import time
 import imutils
 
 import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import Image
+from navigation.msg import navigation
+
+class robotInfo:
+    def __init__(self, id):
+        self.id = id
+        self.pose = np.array([0.0, 0.0])
+        rospy.Subscriber("/volta_" + str(self.id) + "/amcl_pose", PoseWithCovarianceStamped, self.poseCallback)
+
+    def poseCallback(self, msg):
+        self.pose[0] = msg.pose.pose.position.x
+        self.pose[1] = msg.pose.pose.position.y
 
 class Camera:
     def __init__(self, noOfRobots):
@@ -32,6 +44,25 @@ class Camera:
                     self.robotsAroundMe.add(id[0])
         self.frontCamera = True
 
+    def selectMyLeader(self, robotsInfo):
+        while not self.frontCamera and self.leftCamera and self.rightCamera:
+            pass
+        self.frontCamera = False
+        self.leftCamera = False
+        self.rightCamera = False
+
+        minDistance = np.inf
+        myLeaderID = np.nan
+        for robot in self.robotsAroundMe:
+            if robot == 0:
+                return 0
+            distance = np.linalg.norm(robotsInfo[robot].pose - robotsInfo[robotID].pose)
+            if distance < minDistance:
+                minDistance = distance
+                myLeaderID = robot
+        self.robotsAroundMe = set({})
+        return myLeaderID
+
 if __name__ == '__main__':
     try:
         robotID = int(rospy.myargv(argv=sys.argv)[1])
@@ -41,15 +72,20 @@ if __name__ == '__main__':
         camera = Camera(noOfRobots)
 
         rospy.Subscriber("/volta_" + str(robotID) + "/camera_front_ir/camera_front/color/image_raw", Image, camera.frontCameraCallback)
+
+        robotsInfo = []
+        for id in range(noOfRobots):
+            robotsInfo.append(robotInfo(id))
+
         time.sleep(1)
 
         while not rospy.is_shutdown():
             if camera.frontCamera:
                 robotsAroundMeCopy = camera.robotsAroundMe.copy()
-                print("robotsAroundMeCopy", robotsAroundMeCopy)
+                print(robotsAroundMeCopy)
                 camera.frontCamera = False
-                robotsAroundMe = set({})
                 robotsAroundMeCopy = set({})
+                robotsAroundMe = set({})
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
